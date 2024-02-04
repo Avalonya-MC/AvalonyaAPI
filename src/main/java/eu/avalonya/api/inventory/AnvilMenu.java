e81973ba4b83e98b980ce0903ce056cf18d9561d
Nirbose <62440475+Nirbose@users.noreply.github.com>
2024-02-04 01:05:18 +0100

    fix AnvilMenu

--- src/main/java/eu/avalonya/api/inventory/AnvilMenu.java
+++ src/main/java/eu/avalonya/api/inventory/AnvilMenu.java
@@ -1,31 +1,44 @@
 package eu.avalonya.api.inventory;
 
+import eu.avalonya.api.AvalonyaAPI;
 import eu.avalonya.api.items.CustomItemStack;
-import fr.mrmicky.fastinv.FastInv;
+import net.wesjd.anvilgui.AnvilGUI;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 
-public abstract class AnvilMenu extends FastInv {
+import java.util.List;
+
+public abstract class AnvilMenu {
 
     public final static ItemStack FIRST_SLOT_ITEM = new CustomItemStack(Material.PAPER, " ");
     public final static ItemStack OUTPUT_SLOT_ITEM = new CustomItemStack(Material.PAPER, " ");
 
-    public AnvilMenu(String title) {
-        super(InventoryType.ANVIL, title);
+    private final AnvilGUI.Builder builder;
 
-        AnvilInventory anvilInventory = (AnvilInventory) getInventory();
-
-        anvilInventory.setRepairCost(0);
-        setItem(0, FIRST_SLOT_ITEM, event -> event.setCancelled(true));
-        setItem(2, OUTPUT_SLOT_ITEM, event -> {
-            event.setCancelled(true);
-            onConfirm((Player) event.getWhoClicked(), anvilInventory.getRenameText());
-        });
+    public AnvilMenu(String title, String text) {
+        this.builder = new AnvilGUI.Builder()
+                .title(title)
+                .text(text)
+                .itemLeft(FIRST_SLOT_ITEM)
+                .itemOutput(OUTPUT_SLOT_ITEM)
+                .onClick((slot, stateSnapshot) -> {
+                    return onConfirm(stateSnapshot.getPlayer(), stateSnapshot.getText());
+                })
+                .plugin(AvalonyaAPI.getInstance());
     }
 
-    public abstract void onConfirm(Player player, String text);
+    public abstract List<AnvilGUI.ResponseAction> onConfirm(Player player, String text);
+
+    public void open(Player player) {
+        builder.open(player);
+    }
+
+    public void open(HumanEntity humanEntity) {
+        open((Player) humanEntity);
+    }
 
 }
