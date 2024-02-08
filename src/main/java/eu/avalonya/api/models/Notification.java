27bd5cacbe986d45bb309dc0cfaae4084dd17ab2
Nirbose <62440475+Nirbose@users.noreply.github.com>
2024-02-08 12:20:22 +0100

    change createdAt type && add comments

--- src/main/java/eu/avalonya/api/models/Notification.java
+++ src/main/java/eu/avalonya/api/models/Notification.java
@@ -2,12 +2,18 @@ package eu.avalonya.api.models;
 
 import org.bukkit.entity.Player;
 
+import java.util.Date;
+
 public class Notification {
 
     private final String message;
     private final int delay;
-    private final long createdAt = System.currentTimeMillis();
+    private final Date createdAt = new Date();
 
+    /**
+     * Crée une notification sans delay de reponse
+     * @param message message à envoyer
+     */
     public Notification(String message) {
         this(message, 0);
     }
@@ -30,12 +36,17 @@ public class Notification {
         return delay;
     }
 
-    public long getCreatedAt() {
+    public Date getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Vérifie si la notification est expirée
+     * @return true si la notification est expirée
+     */
     public boolean isExpired() {
-        return System.currentTimeMillis() - createdAt > delay && delay > 0;
+        // Si le delay est supérieur à 0 et que le temps actuel moins le temps de création est supérieur au delay
+        return delay > 0 && new Date().getTime() - createdAt.getTime() > delay * 1000L;
     }
 
     public void sendTo(Player player) {
