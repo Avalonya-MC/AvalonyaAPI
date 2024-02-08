f7d14923ec97b3a8dd833846f4a1f45a1df12687
Nirbose <62440475+Nirbose@users.noreply.github.com>
2024-02-08 12:29:54 +0100

    add expired task

--- src/main/java/eu/avalonya/api/AvalonyaAPI.java
+++ src/main/java/eu/avalonya/api/AvalonyaAPI.java
@@ -5,16 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class AvalonyaAPI extends JavaPlugin
 {
 
+    private static AvalonyaAPI instance;
+
     @Override
     public void onEnable()
     {
-
+        instance = this;
     }
 
     @Override
     public void onDisable()
     {
 
+    }
+
+    public static AvalonyaAPI getInstance()
+    {
+        return instance;
     }
 
 }
--- src/main/java/eu/avalonya/api/models/Notification.java
+++ src/main/java/eu/avalonya/api/models/Notification.java
@@ -1,14 +1,19 @@
 package eu.avalonya.api.models;
 
+import eu.avalonya.api.AvalonyaAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.Date;
 
 public class Notification {
 
     private final String message;
+    private String expireMessage;
     private final int delay;
     private final Date createdAt = new Date();
+    private BukkitTask task;
 
     /**
      * Crée une notification sans delay de reponse
@@ -24,12 +29,31 @@ public class Notification {
      * @param delay delay de reponse en secondes
      */
     public Notification(String message, int delay) {
+        this(message, null, delay);
+    }
+
+    /**
+     * Crée une notification avec un message d'expiration
+     * @param message message à envoyer
+     * @param expireMessage message à envoyer si la notification est expirée
+     * @param delay delay de reponse en secondes
+     */
+    public Notification(String message, String expireMessage, int delay) {
         this.message = message;
+        this.expireMessage = expireMessage;
         this.delay = delay;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public String getExpireMessage() {
+        return expireMessage;
+    }
+
+    public String setExpireMessage(String expireMessage) {
+        return this.expireMessage = expireMessage;
     }
 
     public int getDelay() {
@@ -50,13 +74,21 @@ public class Notification {
     }
 
     public void sendTo(Player player) {
-        if (!isExpired())
+        if (!isExpired()) {
+            task = Bukkit.getScheduler().runTaskLater(AvalonyaAPI.getInstance(), () -> player.sendMessage(expireMessage), delay * 20L);
             player.sendMessage(message);
+        }
     }
 
     public void sendTo(Iterable<Player> players) {
         for (Player player : players) {
             sendTo(player);
+        }
+    }
+
+    public void destroy() {
+        if (task != null) {
+            task.cancel();
         }
     }
 
