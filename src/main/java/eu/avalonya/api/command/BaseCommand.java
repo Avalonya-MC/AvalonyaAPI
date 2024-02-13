261ea042eee5e312dae516d44310ea5f5bbe4610
Nirbose <nirbose2@gmail.com>
2024-02-13 23:14:18 +0100

    change constructor params && update onCommand logic

--- src/main/java/eu/avalonya/api/command/BaseCommand.java
+++ src/main/java/eu/avalonya/api/command/BaseCommand.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,8 +18,7 @@ public abstract class BaseCommand implements CommandExecutor {
      */
     private final Component ERROR_OCCURED = Component.text("Une erreur est survenue lors de l'éxécution de cette commande.").color(NamedTextColor.RED);
     private final Component INSUFFICIENT_PERMISSION = Component.text("Vous n'avez pas accès à cette commande.").color(NamedTextColor.RED);
-    private final Component NO_CONSOLE = Component.text("Vous ne pouvez pas exécuter cette commande à partir de la commande.").color(NamedTextColor.RED);
-    private final Component ONLY_CONSOLE = Component.text("Cette commande ne peut être exécutée qu'à partir de la console.").color(NamedTextColor.RED);
+    private final Component INVALID_SENDER_TYPE = Component.text("Vous n'avez pas accès à cette commande.").color(NamedTextColor.RED);
 
     /**
      * Variables
@@ -31,27 +31,27 @@ public abstract class BaseCommand implements CommandExecutor {
      */
 
     public BaseCommand() {
-        this("", null);
+        this(null, SenderType.ALL);
     }
 
     public BaseCommand(String permission) {
-        this(permission, null);
+        this(permission, SenderType.ALL);
     }
 
     public BaseCommand(SenderType senderType) {
-        this("", senderType);
+        this(null, senderType);
     }
 
 
-    public BaseCommand(String[] permissions) {
-        this(permissions, null);
+    public BaseCommand(String ...permissions) {
+        this(SenderType.ALL, permissions);
     }
 
     public BaseCommand(String permission, SenderType senderType) {
-        this(new String[]{permission}, senderType);
+        this(senderType, permission);
     }
 
-    public BaseCommand(String[] permissions, SenderType senderType) {
+    public BaseCommand(SenderType senderType, String ...permissions) {
         this.senderType = senderType;
         this.permissions = permissions;
     }
@@ -61,39 +61,60 @@ public abstract class BaseCommand implements CommandExecutor {
     }
 
     /**
-     * Call the abstract method {@link BaseCommand#onCommand(CommandSender, SenderType, String[])}
+     * Call the abstract method {@link BaseCommand#run(CommandSender, SenderType, String[])}
      */
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, String[] strings) {
-        SenderType senderType = (sender instanceof Player) ? SenderType.PLAYER : SenderType.CONSOLE;
-        if (this.senderType != null && this.senderType != senderType) {
-            sender.sendMessage(senderType == SenderType.PLAYER ? ONLY_CONSOLE : NO_CONSOLE);
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, String[] strings)
+    {
+        final SenderType type = SenderType.fromInstance(sender);
+
+        if (this.senderType != SenderType.ALL && this.senderType != type)
+        {
+            sender.sendMessage(INVALID_SENDER_TYPE);
             return true;
         }
 
-        boolean hasPerm = Arrays.stream(permissions).anyMatch(p -> p.isEmpty() || sender.hasPermission(p) || (p.equalsIgnoreCase("OP") && sender.isOp()));
+        // Si p == null ou le joueur a la permission ou le joueur est op alors on retourne true
+        boolean hasPerm = Arrays.stream(permissions).anyMatch(p -> p == null || sender.hasPermission(p) || sender.isOp());
 
-        if (!hasPerm) { // Does not have access to this command
+        if (!hasPerm)
+        {
             sender.sendMessage(INSUFFICIENT_PERMISSION);
-            return false;
+            return true;
         }
 
-        try { // Execute the command
-            onCommand(sender, senderType, strings);
-        } catch(Exception e) { // An error happened
-            e.printStackTrace(); // TODO: Utiliser un Logger (éventuellement)
+        try
+        {
+            run(sender, senderType, strings);
+        }
+        catch(Exception e)
+        {
             sender.sendMessage(ERROR_OCCURED);
         }
         return true;
     }
 
-    public abstract void onCommand(CommandSender sender, SenderType senderType, String[] args);
+    public abstract void run(CommandSender sender, SenderType senderType, String[] args);
 
     public enum SenderType {
 
+        NONE,
         PLAYER,
-        CONSOLE
+        CONSOLE,
+        ALL;
+
+        public static SenderType fromInstance(CommandSender sender) {
+            if (sender instanceof Player)
+            {
+                return PLAYER;
+            }
+            else if (sender instanceof ConsoleCommandSender)
+            {
+                return CONSOLE;
+            }
+            return NONE;
+        }
 
     }
 
