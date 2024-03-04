e3bedcde9ecf60a633b8ccc86b35b60f35a18c09
Nirbose <nirbose2@gmail.com>
2024-03-04 21:28:53 +0100

    change primary key

--- src/main/java/eu/avalonya/api/sql/migrations/V06_AddCitizensTable.java
+++ src/main/java/eu/avalonya/api/sql/migrations/V06_AddCitizensTable.java
@@ -7,14 +7,15 @@ public class V06_AddCitizensTable extends Migration
 
     private String addCitizensTable = """
             CREATE TABLE `citizens` (
-                `id` INT NOT NULL AUTO_INCREMENT,
+                `player_uuid` VARCHAR(255) NOT NULL,
                 `town_id` INT DEFAULT NULL,
                 `role_id` INT NOT NULL DEFAULT 0,
                 `money` FLOAT NOT NULL DEFAULT 0,
                 `joined_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                PRIMARY KEY (`id`),
+                PRIMARY KEY (`player_uuid`),
                 FOREIGN KEY (`town_id`) REFERENCES `towns`(`id`),
-                FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`)
+                FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`),
+                FOREIGN KEY (`player_uuid`) REFERENCES `player` (`uuid`)
             );
             """;
 
