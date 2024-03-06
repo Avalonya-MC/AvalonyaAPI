578c13f131235c9142bcb4399dd9b0826b6dfd85
Nirbose <nirbose2@gmail.com>
2024-03-06 14:52:46 +0100

    add plots table mapping

--- src/main/java/eu/avalonya/api/sql/migrations/MigrationMapping.java
+++ src/main/java/eu/avalonya/api/sql/migrations/MigrationMapping.java
@@ -11,7 +11,8 @@ public enum MigrationMapping
     V03_ADD_PLAYER_INFO(3, V03_AddPlayerInfo.class),
     V04_ADD_TOWNS_TABLE(4, V04_AddTownsTable.class),
     V05_ADD_ROLES_TABLE(5, V05_AddRolesTable.class),
-    V06_ADD_CITIZENS_TABLE(6, V06_AddCitizensTable.class);
+    V06_ADD_CITIZENS_TABLE(6, V06_AddCitizensTable.class),
+    V07_ADD_PLOTS_TABLE(7, V07_AddPlotsTable.class),;
 
     private final int id;
     private final Class<? extends Migration> migrationClass;
