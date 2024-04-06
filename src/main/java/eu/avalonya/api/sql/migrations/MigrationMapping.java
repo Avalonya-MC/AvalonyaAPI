d3f6a3d70862635b0605569dc529fc1ad917c4c0
Alexandre Barde <alexandre@brde.fr>
2024-04-06 11:58:14 -0400

    Add mariage table + allies in migration

--- src/main/java/eu/avalonya/api/sql/migrations/MigrationMapping.java
+++ src/main/java/eu/avalonya/api/sql/migrations/MigrationMapping.java
@@ -15,6 +15,8 @@ public enum MigrationMapping
     V07_ADD_PLOTS_TABLE(7, V07_AddPlotsTable.class),
     V08_UPDATE_CITIZENS_ROLE(8, V08_UpdateCitizensRole.class),
     V09_UPDATE_CITIZENS_TABLE(9, V09_UpdateCitizensTable.class),
+    V10_ADD_ALLIES_TABLE(10, V10_AddAllies.class),
+    v11_ADD_MARIAGE_TABLE(11, V11_AddMariageTable.class)
     ;
 
     private final int id;
---
+++ src/main/java/eu/avalonya/api/sql/migrations/V11_AddMariageTable.java
@@ -0,0 +1,26 @@
+package eu.avalonya.api.sql.migrations;
+
+import eu.avalonya.api.sql.Migration;
+
+public class V11_AddMariageTable extends Migration
+{
+
+    private final String addMariageTable = """
+            CREATE TABLE `mariage` (
+              `id` INT NOT NULL AUTO_INCREMENT,
+              `sender` INT NOT NULL,
+              `receiver` INT NOT NULL,
+              `create_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+              FOREIGN KEY (`sender`) REFERENCES `citizens`(`id`),
+              FOREIGN KEY (`receiver`) REFERENCES `citizens`(`id`),
+              PRIMARY KEY (`id`)
+            );
+            """;
+
+    @Override
+    public void execute()
+    {
+        this.execute(addMariageTable);
+    }
+
+}
