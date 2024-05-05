10b6339d775346ce532136562bf26142dd9c37cb
Alexandre Barde <alexandre@brde.fr>
2024-05-05 09:34:24 -0400

    Fix table

--- src/main/java/eu/avalonya/api/sql/migrations/V11_AddMariageTable.java
+++ src/main/java/eu/avalonya/api/sql/migrations/V11_AddMariageTable.java
@@ -8,8 +8,8 @@ public class V11_AddMariageTable extends Migration
     private final String addMariageTable = """
             CREATE TABLE `mariage` (
               `id` INT NOT NULL AUTO_INCREMENT,
-              `sender` INT NOT NULL,
-              `receiver` INT NOT NULL,
+              `sender_id` INT NOT NULL,
+              `receiver_id` INT NOT NULL,
               `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
               FOREIGN KEY (`sender_id`) REFERENCES `citizens`(`id`),
               FOREIGN KEY (`receiver_id`) REFERENCES `citizens`(`id`),
