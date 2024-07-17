649780c4210fd79c8d4970a9fdbe490719dc84af
Daweii-dev <101587914+Daweii-dev@users.noreply.github.com>
2024-07-17 19:01:17 +0200

    corection bug

--- src/main/java/eu/avalonya/api/command/DemoCommand.java
+++ src/main/java/eu/avalonya/api/command/DemoCommand.java
@@ -5,6 +5,7 @@ import eu.avalonya.api.command.arguments.IntegerArgument;
 import eu.avalonya.api.command.arguments.PlayerArgument;
 import eu.avalonya.api.command.arguments.RegexArgument;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.block.CommandBlock;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
@@ -18,6 +19,7 @@ public class DemoCommand extends BaseCommand<Player>
         // Add sub commands
         addSubCommand(BaseCommand.newSubCommand("sub", (sender, args) -> {
             sender.sendMessage("sub command");
+            CommandBlock player = (CommandBlock) sender;
         }));
         addSubCommand(sub2());
 
@@ -26,6 +28,7 @@ public class DemoCommand extends BaseCommand<Player>
         addArgument(BooleanArgument.class, true);
         addArgument(new RegexArgument("[a-zA-Z_]+", true));
     }
+
 
     @Override
     public void run(Player sender, ArgumentCollection args)
