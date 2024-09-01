8ba14c7cb2041a284b5e90f3558040ed5f95783f
Nirbose <nirbose2@gmail.com>
2024-09-01 16:17:11 +0200

    test base and fix Authorization

--- src/main/java/eu/avalonya/api/http/Backend.java
+++ src/main/java/eu/avalonya/api/http/Backend.java
@@ -7,47 +7,46 @@ import lombok.SneakyThrows;
 
 public class Backend {
 
-    public final static String BACKEND_URL = ConfigFilesManager.getFile("backend").get().getString("url");
+    public final static String BACKEND_URL = ConfigFilesManager.getFile("backend").get()
+        .getString("url");
 
-    public static String get(Endpoint endpoint, String body)
-    {
+    public static Response get(Endpoint endpoint, String body) {
         return request("GET", endpoint, body);
     }
 
-    public static String post(Endpoint endpoint, String body)
-    {
+    public static Response post(Endpoint endpoint, String body) {
         return request("POST", endpoint, body);
     }
 
-    public static String put(Endpoint endpoint, String body)
-    {
+    public static Response put(Endpoint endpoint, String body) {
         return request("PUT", endpoint, body);
     }
 
-    public static String delete(Endpoint endpoint, String body)
-    {
+    public static Response delete(Endpoint endpoint, String body) {
         return request("DELETE", endpoint, body);
     }
 
     @SneakyThrows
-    public static String request(String method, Endpoint endpoint, String body)
-    {
+    public static Response request(String method, Endpoint endpoint, String body) {
         URL url = new URL(BACKEND_URL + endpoint.getPath());
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setRequestMethod(method);
         con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization",
+            "Bearer " + ConfigFilesManager.getFile("backend").get().getString("token"));
         con.setDoOutput(true);
 
         con.getOutputStream().write(body.getBytes());
         con.getOutputStream().flush();
         con.getOutputStream().close();
 
-        String response = con.getResponseMessage();
+        int status = con.getResponseCode();
+        String response = new String(con.getInputStream().readAllBytes());
 
         con.disconnect();
 
-        return response;
+        return new Response(status, response);
     }
 
 }
--- src/main/java/eu/avalonya/api/models/Model.java
+++ src/main/java/eu/avalonya/api/models/Model.java
@@ -1,30 +1,51 @@
 package eu.avalonya.api.models;
 
+import eu.avalonya.api.AvalonyaAPI;
 import eu.avalonya.api.http.Backend;
 import eu.avalonya.api.http.Endpoint;
+import eu.avalonya.api.http.Response;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public abstract class Model<T extends Model<?>> {
 
     public abstract Endpoint getEndpoint();
 
-    public static <T extends Model<?>> T create(Class<T> clazz, Map<String, Object> data) {
+    public static <T extends Model<?>> T create(Class<T> clazz, Map<String, ?> data) {
         String body = "{" + data.entrySet().stream()
             .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
             .reduce((a, b) -> a + "," + b).orElse("") + "}";
 
-        String response = Backend.post(clazz.cast(null).getEndpoint(), body);
+        Response response = null;
+        try {
+            response = Backend.post(clazz.getConstructor().newInstance().getEndpoint(), body);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
 
-        return null; // TODO: Create instance of T from response
+        if (response.getStatus() != 200) {
+            return null;
+        }
+
+        return AvalonyaAPI.getGson().fromJson(response.getBody(), clazz);
     }
 
-    public static <T extends Model<?>> T find(Class<T> clazz, int id) {
-        // TODO: Implement this method
-        return null;
+    public static <T extends Model<?>> T find(Class<T> clazz, String id) {
+        Endpoint endpoint = clazz.cast(null).getEndpoint();
+        Response response = Backend.get(Endpoint.bind(endpoint, id), "");
+
+        if (response.getStatus() != 200) {
+            return null;
+        }
+
+        return AvalonyaAPI.getGson().fromJson(response.getBody(), clazz);
     }
 
     public void save() {
-        Backend.put(getEndpoint(), "{}"); // TODO: Implement this method
+        Backend.put(getEndpoint(), AvalonyaAPI.getGson().toJson(this));
     }
 
 }
