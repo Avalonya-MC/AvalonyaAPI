badb47cec7bac7ef7444d4fb79d2a1e50b178791
Nirbose <62440475+Nirbose@users.noreply.github.com>
2024-10-03 16:01:54 +0200

    refacto code and fix arg null if empty

--- src/main/java/eu/avalonya/api/command/ArgumentCollection.java
+++ src/main/java/eu/avalonya/api/command/ArgumentCollection.java
@@ -22,14 +22,10 @@ public class ArgumentCollection implements Iterable<Argument<?>>
 
     public Argument<?> get(int index)
     {
-        try
-        {
-            return arguments.get(index);
-        }
-        catch (IndexOutOfBoundsException e)
-        {
+        if (index < 0 || index >= arguments.size()) {
             return null;
         }
+        return arguments.get(index);
     }
 
     public <T> T get(int index, Class<T> type)
@@ -59,6 +55,10 @@ public class ArgumentCollection implements Iterable<Argument<?>>
     public void addRest(String rest)
     {
         this.rest.add(rest);
+    }
+
+    public void clear() {
+        arguments.clear();
     }
 
     /**
